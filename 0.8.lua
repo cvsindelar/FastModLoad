15299f358c7049368077b474b3ef56cd2cd9b520
cvsindelar <charles.sindelar@yale.edu>
2026-03-30 07:37:35 -0400

    modulefile: Revise description and update homepage URL
    Updated module loader description and homepage link.

--- 0.8.lua
+++ 0.8.lua
@@ -2,11 +2,11 @@ help([==[
 
 Description
 ===========
-Module Loader-Quick (for lmod)
+Fast Module Loader for Lmod
 
 More information
 ================
- - Homepage: https://github.com/cvsindelar/mlq
+ - Homepage: https://github.com/cvsindelar/fml
 ]==])
 
 whatis([==[Description: Fast Module Loader for Lmod-Based HPC Software Stacks]==])
@@ -25,18 +25,4 @@ if myShellType() == "sh" then
 
 else
   execute {cmd="echo 'FML: sorry, this is not implemented for shell type ".. myShellType() .. "' >&2", modeA={"load"}}
-
---   -- tcsh/sh: do the corresponding things with aliases
---   -- execute {cmd="echo alias module 'eval `bash " .. script .. "\!*`'", modeA={"load"}}
---   -- persuade lua to execute the following csh command:
---   --    alias module 'eval `bash fml.sh \!*`'
---   -- To do: 'ml reset' fails due to '__fml_orig_module: Command not found' (2x)
---   execute {cmd="set __fml_orig_module_code=\"`alias module`\"", modeA={"load"}}
---   execute {cmd="alias __fml_orig_module \"$__fml_orig_module_code\"", modeA={"load"}}
---   execute {cmd="alias module 'eval `bash " .. script .. " csh \\!*`'", modeA={"load"}}
---   
---   execute {cmd ="unalias module", modeA = {"unload"}}
---   execute {cmd="alias module \"$__fml_orig_module_code\"", modeA={"unload"}}
---   execute {cmd="unset __fml_orig_module_code", modeA={"unload"}}
---   execute {cmd="unalias __fml_orig_module", modeA={"unload"}}
 end
